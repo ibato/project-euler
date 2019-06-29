e16970caae9894672712f43d77bb5d71e015f8f5
ibato <yoonhaerim@gmail.com>
2019-06-30 00:01:05 +0900

    Fix indentations

--- python/test_001.py
+++ python/test_001.py
@@ -1,12 +1,15 @@
 from time import time
 
+
 def solve():
     return sum(filter(lambda x: x % 3 == 0 or x % 5 == 0, range(1, 1000)))
+
 
 def test():
     answer = solve()
     print("Answer:", answer)
     assert answer == 233168
+
 
 if __name__ == '__main__':
     start = time()
--- python/test_002.py
+++ python/test_002.py
@@ -1,21 +1,24 @@
 from time import time
 
+
 def solve():
-  a = 1
-  b = 2
-  summation = b
-  while a + b < 4000000:
-    c = a + b
-    if c % 2 == 0:
-      summation = summation + c
-    a = b
-    b = c
-  return summation
+    a = 1
+    b = 2
+    summation = b
+    while a + b < 4000000:
+        c = a + b
+        if c % 2 == 0:
+            summation = summation + c
+        a = b
+        b = c
+    return summation
+
 
 def test():
     answer = solve()
     print("Answer:", answer)
     assert answer == 4613732
+
 
 if __name__ == '__main__':
     start = time()
