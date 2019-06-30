c770e90113a313902e5d979343d7ff2d93cda58c
ibato <yoonhaerim@gmail.com>
2019-06-30 14:10:24 +0900

    Update the notebook 001

--- notebook/py/001.py
+++ notebook/py/001.py
@@ -2,8 +2,9 @@
 ## Multiples of 3 and 5
 # https://projecteuler.net/problem=1
 # 
-# > If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.\n",
-# > Find the sum of all the multiples of 3 or 5 below 1000."
+# > If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+# > 
+# > Find the sum of all the multiples of 3 or 5 below 1000.
 
 #%% [markdown]
 # It's just the brute force attack.
