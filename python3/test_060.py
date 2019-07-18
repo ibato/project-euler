b0f65d08cc8039504374958019f478a205d09db1
ibato <yoonhaerim@gmail.com>
2019-07-19 06:28:19 +0900

    Add test_060.py (wip)

--- python3/test_060.py
+++ python3/test_060.py
@@ -5,32 +5,35 @@ import test_runner as runner
 
 
 cache = []
-primes_100000 = prm.find_primes(2, 100000)
+start = time()
+
+primes_sieve = prm.find_primes(2, 100000000)
 primes = prm.find_primes(2, 10000)
 
 def solve():
     primes.remove(2)
     primes.remove(5)
 
-    start = time()
-
     for a in range(len(primes)):
         for b in range(a + 1, len(primes)):
+            if not can_produce_prime(primes[a], primes[b]):
+                continue
+
             for c in range(b + 1, len(primes)):
+                if not can_produce_prime(primes[a], primes[c]) or not can_produce_prime(primes[b], primes[c]):
+                    continue
+
                 for d in range(c + 1, len(primes)):
+                    if not can_produce_prime(primes[a], primes[d]) or not can_produce_prime(primes[b], primes[d]) or not can_produce_prime(primes[c], primes[d]):
+                        continue
+
                     for e in range(d + 1, len(primes)):
                         n1 = primes[a]; n2 = primes[b]; n3 = primes[c]; n4 = primes[d]; n5 = primes[e]
-
-                        current = time()
-                        if (current - start) % 10 >= 0 and (current - start) % 10 < 0.001:
-                            print("log: %s, %s" % (format_time(current - start), (n1, n2, n3, n4, n5)))
-
-                        for cb in combinations((n1, n2, n3, n4, n5), 2):
-                            if cb not in cache:
-                                if not can_produce_prime(cb[0], cb[1]):
-                                    break
-                        else:
+                        if can_produce_prime(n1, n5) and can_produce_prime(n2, n5) and can_produce_prime(n3, n5) and can_produce_prime(n4, n5):
+                            print("answer: ", sum((n1, n2, n3, n4, n5)), (n1, n2, n3, n4, n5))
                             return sum((n1, n2, n3, n4, n5))
+                        else:
+                            continue
 
 
 def format_time(seconds):
@@ -42,10 +45,18 @@ def format_time(seconds):
 
 
 def can_produce_prime(n1, n2):
-    a1 = int(str(n1) + str(n2))
-    a2 = int(str(n1) + str(n2))
+    current = time()
+    if (current - start) % 10 >= 0 and (current - start) % 10 < 0.1:
+        print("log: %s, %s" % (format_time(current - start), (n1, n2)))
 
-    if a1 in primes_100000 and a2 in primes_100000:
+    if (n1, n2) in cache:
+        return True
+
+    a1 = int(str(n1) + str(n2))
+    a2 = int(str(n2) + str(n1))
+
+    if a1 in primes_sieve and a2 in primes_sieve:
+        print(a1, a2, n1, n2)
         cache.append((n1, n2))
         return True
     return False
